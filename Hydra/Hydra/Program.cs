3319dc728b7f5ca01b8fae468bf0c834d755c260
Delkarix <40216192+Delkarix@users.noreply.github.com>
2019-05-10 12:25:55 -0400

    Add files via upload

---
+++ Hydra/Hydra/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using System.Windows.Forms;
+
+class Program
+{
+    static void Main()
+    {
+        MessageBox.Show("Cut off a head, two more take its place.", "Hydra", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+        Thread thread1 = new Thread(Main);
+        Thread thread2 = new Thread(Main);
+        thread1.Start();
+        thread2.Start();
+    }
+}
